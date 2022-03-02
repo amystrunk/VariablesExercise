f6a453ad0c258bd8b59bc85efb0bb1fb2331c91b
Strunk <strunk@MacBook-Pro.local>
2022-03-02 10:01:52 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// 1
+// const
+// 2
+let num = 20;
+// 3
+num = 15;
+// 4
+num += 7; 
+// 5
+num--;
+// 6
+num= "This is not a number";
+// 7
+let blank;
+// 8
+console.log(blank);
+// 9
+blank = null;
+// bonus
+// 1
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+// const [quarter, dime, nickel, penny] = [0.25, 0.10, 0.05, 0.01]
+// 2
+console.log(quarter, dime, nickel, penny);
+// 3
+let letters = "qwertyuiop";
+// 4
+console.log(letters.length);
+console.log(letters[9])
+// OR
+// console.log(letters.slice(9)); 
+// or
+// console.log(letters.slice(-1))
+// OR
+// takes length of letters minus one postion, which gives us P
+// console.log(letters[letters.length - 1]); 
